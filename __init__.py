8d08fbcb4c4ee20ef7cc5f74534b92349065ea83
Zhiyuan Chen <zc@int.ac.cn>
2019-07-11 14:24:04 +0800

    Rename package

--- __init__.py
+++ __init__.py
@@ -9,5 +9,5 @@ from keras import layers
 from keras.layers import Conv2D, MaxPooling2D, Input
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.models import Model
-from hand_recognizer.hand_recognizer.utils import detector_utils as detector_utils
-from hand_recognizer.hand_recognizer import *
+from hand_classifier.algorithm.utils import detector_utils as detector_utils
+from hand_classifier.algorithm import *
