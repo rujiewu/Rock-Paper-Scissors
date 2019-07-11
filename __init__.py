d7a00a9fabdb260c4ef61416ecde6aa95843c233
Ti-ruo Yan <yantiruo@gmail.com>
2019-07-11 11:01:10 +0800

    .

--- __init__.py
+++ __init__.py
@@ -2,14 +2,12 @@ import argparse
 import datetime
 import operator
 import cv2
+
 import numpy as np
 from keras import backend as K
 from keras import layers
 from keras.layers import Conv2D, MaxPooling2D, Input
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.models import Model
-from hand_recognizer import *
-from hand_recognizer.utils import detector_utils as detector_utils
-
-
-import package
+from hand_recognizer.hand_recognizer.utils import detector_utils as detector_utils
+from hand_recognizer.hand_recognizer import *
